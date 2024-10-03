bc41153046eb6d25457582b5d1dc37ef366a7a34
Uday <uday1549.be23@chitkarauniversity.edu.in>
2024-10-03 09:09:43 +0530

    Delete Task_clickImageRemove directory

--- Task_clickImageRemove/index.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('#images').addEventListener('click', function(e){
-    console.log(e.target.tagName);
-    if (e.target.tagName === 'IMG') {
-        console.log(e.target.id);
-        let removeIt = e.target.parentNode
-        removeIt.remove()
-    }
-})
